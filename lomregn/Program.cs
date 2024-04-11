c5673f8405db8bfe86d71670c575c599743c5a30
Christian Nielsen <120013949+HrSejer@users.noreply.github.com>
2024-04-11 13:43:06 +0200

    j

--- lomregn/Program.cs
+++ lomregn/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace lomregn
@@ -11,6 +10,7 @@ namespace lomregn
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -102,6 +102,8 @@ namespace lomregn
             Thread.Sleep(10000);
             Console.WriteLine("Rytter er fed og sutter bearnaise");
 >>>>>>> parent of 15dbe5b (gange)
+=======
+>>>>>>> parent of e89b7dd (tEST)
         }
     }
 }
