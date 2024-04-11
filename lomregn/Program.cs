a69e4006ebe10caaa75eb6d5c86369eea4403de4
Andreask12345 <And3n11@gmail.com>
2024-04-11 13:49:28 +0200

    FIx

--- lomregn/Program.cs
+++ lomregn/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace lomregn
@@ -10,6 +11,66 @@ namespace lomregn
     {
         static void Main(string[] args)
         {
+            bool mere = true;
+
+            while (mere)
+            {
+                string hahah;
+                int svar;
+                Console.WriteLine("Velkommen til lommeregner. Vil du gerne *(gange), +(plusse), -(minus) eller /(dividere)?");
+
+                string d = Console.ReadLine();
+
+                Console.WriteLine("Vælg dit første tal");
+
+                int tal1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Vælg dit andet tal");
+
+                int tal2 = Convert.ToInt32(Console.ReadLine());
+
+                hahah = "Hjælp mig ";
+
+                if (d == "-")
+                {
+                    svar = tal1 - tal2;
+                    Console.WriteLine("Svaret er " + svar);
+                    Console.WriteLine(hahah);
+                }
+
+                if (d == "*")
+                {
+                    svar = tal1 * tal2;
+                    Console.WriteLine("Svaret er " + svar);
+                }
+
+                if (d == "+")
+                {
+                    svar = tal1 + tal2;
+                    Console.WriteLine("Svaret er " + svar);
+                }
+
+                if (d == "/")
+                {
+                    svar = tal1 / tal2;
+                    Console.WriteLine("Svaret er " + svar);
+                }
+
+
+                Console.WriteLine("Igen (Ja) (Nej)");
+                string igen = Console.ReadLine();
+
+                if (igen == "Ja")
+                {
+                    Console.Clear();
+                }
+
+                else if (igen == "Nej")
+                {
+                    mere = false;
+                }
+
+            }
         }
     }
 }
