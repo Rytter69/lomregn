15dbe5bba784e17366938fe7bc56ea0002e8c9be
Rytter69 <marcusrytterolesen@gmail.com>
2024-04-11 11:14:55 +0200

    gange

--- lomregn/Program.cs
+++ lomregn/Program.cs
@@ -11,9 +11,19 @@ namespace lomregn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Thread.Sleep(10000);
-            Console.WriteLine("Rytter er fed og sutter bearnaise");
+            int svar;
+            Console.WriteLine("Velkommen til lommeregner. Vil du gerne *(gange), +(plusse), -(minus) eller /(dividere)?");
+            string d = Console.ReadLine();
+            Console.WriteLine("Vælg dit første tal");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vælg dit andet tal");
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+            if (d == "*")
+            {
+                svar = tal1 * tal2;
+                Console.WriteLine("Svaret er " + svar);
+            }
+            Console.ReadKey();
         }
     }
 }
