7f9aa39035e70f719e5df3bf235ac60668c6e3cf
Rytter69 <marcusrytterolesen@gmail.com>
2024-04-11 13:52:43 +0200

    asd

--- lomregn/Program.cs
+++ lomregn/Program.cs
@@ -11,66 +11,19 @@ namespace lomregn
     {
         static void Main(string[] args)
         {
-            bool mere = true;
-
-            while (mere)
+            int svar;
+            Console.WriteLine("Velkommen til lommeregner. Vil du gerne *(gange), +(plusse), -(minus) eller /(dividere)?");
+            string d = Console.ReadLine();
+            Console.WriteLine("Vælg dit første tal");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vælg dit andet tal");
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+            if (d == "*")
             {
-                string hahah;
-                int svar;
-                Console.WriteLine("Velkommen til lommeregner. Vil du gerne *(gange), +(plusse), -(minus) eller /(dividere)?");
-
-                string d = Console.ReadLine();
-
-                Console.WriteLine("Vælg dit første tal");
-
-                int tal1 = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Vælg dit andet tal");
-
-                int tal2 = Convert.ToInt32(Console.ReadLine());
-
-                hahah = "Hjælp mig ";
-
-                if (d == "-")
-                {
-                    svar = tal1 - tal2;
-                    Console.WriteLine("Svaret er " + svar);
-                    Console.WriteLine(hahah);
-                }
-
-                if (d == "*")
-                {
-                    svar = tal1 * tal2;
-                    Console.WriteLine("Svaret er " + svar);
-                }
-
-                if (d == "+")
-                {
-                    svar = tal1 + tal2;
-                    Console.WriteLine("Svaret er " + svar);
-                }
-
-                if (d == "/")
-                {
-                    svar = tal1 / tal2;
-                    Console.WriteLine("Svaret er " + svar);
-                }
-
-
-                Console.WriteLine("Igen (Ja) (Nej)");
-                string igen = Console.ReadLine();
-
-                if (igen == "Ja")
-                {
-                    Console.Clear();
-                }
-
-                else if (igen == "Nej")
-                {
-                    mere = false;
-                }
-
+                svar = tal1 * tal2;
+                Console.WriteLine("Svaret er " + svar);
             }
+            Console.ReadKey();
         }
     }
 }
