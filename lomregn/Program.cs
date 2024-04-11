0b43a801d736aaf2f207dbc069a8db3cff267af5
Christian Sejer Nielsen <pizzacrisp74@gmail.com>
2024-04-11 12:07:33 +0200

    Mere test

--- lomregn/Program.cs
+++ lomregn/Program.cs
@@ -59,7 +59,7 @@ namespace lomregn
                     mere = false;
                 }
 
-                //Test tilbage
+                //Test tilbage jajajajjaja
             }
         }
     }
